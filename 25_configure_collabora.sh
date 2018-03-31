5df73963b3a3f198ab70e37aea7fad0ca3dc2588
Alban VIDAL <alban.vidal@zordhak.fr>
2018-03-31 16:11:59 +0200

    Add « curl » package, Fix arch un repo docker to intall in ARM64

--- 25_configure_collabora.sh
+++ 25_configure_collabora.sh
@@ -60,9 +60,9 @@ lxc exec collabora -- bash -c "DEBIAN_FRONTEND=noninteractive apt-get -y upgrade
 lxc exec collabora -- bash -c "DEBIAN_FRONTEND=noninteractive apt-get -y install vim apt-utils > /dev/null"
  
 echo "$($_ORANGE_)Add Docker repo and install Docker$($_WHITE_)"
-lxc exec collabora -- bash -c "apt-get -y install apt-transport-https gnupg gnupg2 gnupg1 > /dev/null
+lxc exec collabora -- bash -c "apt-get -y install curl apt-transport-https gnupg gnupg2 gnupg1 > /dev/null
                                curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
-                               echo -e '\n# Depôt Docker\ndeb [arch=amd64] https://download.docker.com/linux/debian stretch stable' > /etc/apt/sources.list.d/docker.list
+                               echo -e '\n# Depôt Docker\ndeb https://download.docker.com/linux/debian stretch stable' > /etc/apt/sources.list.d/docker.list
                                apt-get update > /dev/null
                                apt-get -y install docker-ce > /dev/null
                                "
