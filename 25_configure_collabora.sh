433291f5a1f3efe03b0d431837dccfc89f96203c
Alban Vidal <alban.vidal@zordhak.fr>
2018-05-25 15:28:31 +0000

    Send openssl cert generation output to /dev/null
    Signed-off-by: Alban Vidal <alban.vidal@zordhak.fr>

--- 25_configure_collabora.sh
+++ 25_configure_collabora.sh
@@ -88,13 +88,13 @@ lxc exec collabora -- bash -c "
                                mkdir -p /opt/ssl/
                                cd /opt/ssl/
                                mkdir -p certs/ca
-                               openssl genrsa -out certs/ca/root.key.pem 2048
-                               openssl req -x509 -new -nodes -key certs/ca/root.key.pem -days 9131 -out certs/ca/root.crt.pem -subj '/C=DE/ST=BW/L=Stuttgart/O=Dummy Authority/CN=Dummy Authority'
+                               openssl genrsa -out certs/ca/root.key.pem 2048 > /dev/null
+                               openssl req -x509 -new -nodes -key certs/ca/root.key.pem -days 9131 -out certs/ca/root.crt.pem -subj '/C=DE/ST=BW/L=Stuttgart/O=Dummy Authority/CN=Dummy Authority' > /dev/null
                                mkdir -p certs/{servers,tmp}
                                mkdir -p 'certs/servers/localhost'
-                               openssl genrsa -out 'certs/servers/localhost/privkey.pem' 2048 -key 'certs/servers/localhost/privkey.pem'
-                               openssl req -key 'certs/servers/localhost/privkey.pem' -new -sha256 -out 'certs/tmp/localhost.csr.pem' -subj '/C=DE/ST=BW/L=Stuttgart/O=Dummy Authority/CN=localhost'
-                               openssl x509 -req -in certs/tmp/localhost.csr.pem -CA certs/ca/root.crt.pem -CAkey certs/ca/root.key.pem -CAcreateserial -out certs/servers/localhost/cert.pem -days 9131
+                               openssl genrsa -out 'certs/servers/localhost/privkey.pem' 2048 -key 'certs/servers/localhost/privkey.pem' > /dev/null
+                               openssl req -key 'certs/servers/localhost/privkey.pem' -new -sha256 -out 'certs/tmp/localhost.csr.pem' -subj '/C=DE/ST=BW/L=Stuttgart/O=Dummy Authority/CN=localhost' > /dev/null
+                               openssl x509 -req -in certs/tmp/localhost.csr.pem -CA certs/ca/root.crt.pem -CAkey certs/ca/root.key.pem -CAcreateserial -out certs/servers/localhost/cert.pem -days 9131 > /dev/null
                                mv certs/servers/localhost/privkey.pem /etc/loolwsd/key.pem
                                mv certs/servers/localhost/cert.pem /etc/loolwsd/cert.pem
                                mv certs/ca/root.crt.pem /etc/loolwsd/ca-chain.cert.pem
